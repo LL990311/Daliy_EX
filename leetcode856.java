b950216d51c56099bfd4d954611468462d8dbe23
LukeLong <lzx19990311@gmail.com>
2022-10-09 10:26:54 +0800

    upload: leetcode856 括号的分数

--- leetcode856.java
+++ leetcode856.java
@@ -1,3 +1,21 @@
+
+/**
+ * Oct 09 2022
+ * Leetcode_856
+ * Written by Java
+ */
+
+/**
+ * 给定一个平衡括号字符串 S，按下述规则计算该字符串的分数：
+ * () 得 1 分。
+ * AB 得 A + B 分，其中 A 和 B 是平衡括号字符串。
+ * (A) 得 2 * A 分，其中 A 是平衡括号字符串。
+ * 
+ * 栈应用
+ * 时间复杂度: O(n)
+ * 空间复杂度: O(n)
+ */
+
 import java.util.*;
 
 public class leetcode856 {
@@ -5,8 +23,9 @@ public class leetcode856 {
         Deque<Integer> deque = new ArrayDeque<>();
         deque.addLast(0);
         for (char c : s.toCharArray()) {
-            if (c == '(') deque.addLast(0);
-            else{
+            if (c == '(')
+                deque.addLast(0);
+            else {
                 int cur = deque.pollLast();
                 deque.add(deque.pollLast() + Math.max(cur * 2, 1));
             }
