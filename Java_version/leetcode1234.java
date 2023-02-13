42702e9d964a79c4263246e978714fe2aa509511
LukeLong <lzx19990311@gmail.com>
2023-02-13 14:55:50 +0800

    upload leetcode1234.java 替换子串得到平衡字符串

---
+++ Java_version/leetcode1234.java
@@ -0,0 +1,29 @@
+package Java_version;
+
+public class leetcode1234 {
+    public static int balancedString(String s) {
+        int[] cnt = new int[4];
+        String t = "QWER";
+        int n = s.length();
+        for (int i = 0; i < n; ++i) {
+            cnt[t.indexOf(s.charAt(i))]++;
+        }
+        int m = n / 4;
+        if (cnt[0] == m && cnt[1] == m && cnt[2] == m && cnt[3] == m) {
+            return 0;
+        }
+        int ans = n;
+        for (int i = 0, j = 0; i < n; ++i) {
+            cnt[t.indexOf(s.charAt(i))]--;
+            while (j <= i && cnt[0] <= m && cnt[1] <= m && cnt[2] <= m && cnt[3] <= m) {
+                ans = Math.min(ans, i - j + 1);
+                cnt[t.indexOf(s.charAt(j++))]++;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        System.out.println(balancedString("QQQQ")); // 3
+        System.out.println(balancedString("QWQQ")); // 2
+    }
+}
