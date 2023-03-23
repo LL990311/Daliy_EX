52423bbc54ad86e4d37813f18385daa15f39a450
LukeLong <lzx19990311@gmail.com>
2023-03-23 16:06:44 +0800

    upload: leetcode1011 在D天内送达包裹的能力

---
+++ Python_version/leetcode1011.py
@@ -0,0 +1,46 @@
+
+'''
+/**
+ * Mar 23 2023
+ * Leetcode1011
+ * Written by Java
+ * Author: ll
+ */
+
+/**
+ * 传送带上的包裹必须在 days 天内从一个港口运送到另一个港口。
+ * 
+ * 传送带上的第
+ * i 个包裹的重量为 weights[i]。每一天，我们都会按给出重量（weights）的顺序往传送带上装载包裹。我们装载的重量不会超过船的最大运载重量。
+ * 
+ * 返回能在 days 天内将传送带上的所有包裹送达的船的最低运载能力。
+ * 
+ * 时间 O(nlogm)
+ * 空间 O(1)
+ */
+'''
+
+
+def shipWithinDays(self, weights: list[int], days: int) -> int:
+    left, right = max(weights), sum(weights)
+    while left < right:
+        mid = (left + right) >> 1
+        cur, d = 0, 1
+        for w in weights:
+            if cur + w > mid:
+                d += 1
+                cur = 0
+            cur += w
+        if d <= days:
+            right = mid
+        else:
+            left = mid + 1
+    return left
+
+
+def main():
+    print(shipWithinDays(any, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5))
+
+
+if __name__ == '__main__':
+    main()
